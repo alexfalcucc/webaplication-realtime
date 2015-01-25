d57d73df49dbd1ad3769df1c488ad67a92869c11
alexfalcucc <alex.falcucci@gmail.com>
2015-01-25 19:05:39 -0200

    commenting methods

--- node-examples/callback-examples/callback_heaven.js
+++ node-examples/callback-examples/callback_heaven.js
@@ -1,5 +1,12 @@
 var fs = require('fs');
 var readDirectory = function() {
+
+  /*
+    Asynchronous readdir(3). Reads the contents of a directory.
+    The callback gets two arguments (err, files) where files is
+    an array of the names of the files in the directory excluding '.' and '..'.
+  */
+
   fs.readdir(__dirname, function(error, directory) {
     if (error) { throw error; }
     directory.forEach(function(file) {
@@ -10,6 +17,13 @@ var readDirectory = function() {
 
 var read_ = function(file) {
   var path = './' + file;
+
+  /*
+    Objects returned from fs.stat(), fs.lstat() and fs.fstat()
+    and their synchronous counterparts are of this type.
+    See more examples: http://nodejs.org/api/fs.html#fs_class_fs_stats
+  */
+
   fs.stat(path, function(error, stat) {
     if (error) { throw error; }
     if (stat.isFile()) {
