4533375f6e7f89c452d8b9c3e36c3c29cf0d3002
alexfalcucc <alex.falcucci@gmail.com>
2015-01-26 02:11:41 -0200

    add secret key into express-session variable

--- alexsander-ntalk/app.js
+++ alexsander-ntalk/app.js
@@ -8,8 +8,8 @@ var express = require('express')
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(cookieParser('alexsander-ntalk'));
-app.use(expressSession({resave: false, saveUninitialized: false}));
+app.use(cookieParser());
+app.use(expressSession({secret: 'alexsander-ntalk', resave: false, saveUninitialized: false}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
