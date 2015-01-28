e9e626bee6cddefb844d9ca514de679123480c23
alexfalcucc <alex.falcucci@gmail.com>
2015-01-28 02:32:32 -0200

    configuring method override on app.js and create routes to initialize a simple contact CRUD

--- alexsander-ntalk/routes/contacts.js
+++ alexsander-ntalk/routes/contacts.js
@@ -1,4 +1,9 @@
 module.exports = function(app) {
 	var contacts = app.controllers.contacts;
 	app.get('/contacts', contacts.index);
+	app.get('/contact/:id', contacts.show);
+	app.post('/contact', contacts.create);
+	app.get('/contact/:id/editar', contacts.edit);
+	app.put('/contact/:id', contacts.update);
+	app.delete('/contact/:id', contacts.delete);
 };
