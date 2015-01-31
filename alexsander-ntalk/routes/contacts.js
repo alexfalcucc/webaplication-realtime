3b8ccfcdc85cfb712b5c7d3a0f24daa0f772c9d1
alexfalcucc <alex.falcucci@gmail.com>
2015-01-30 23:28:51 -0200

    route DELTE its worked

--- alexsander-ntalk/routes/contacts.js
+++ alexsander-ntalk/routes/contacts.js
@@ -5,7 +5,5 @@ module.exports = function(app) {
 	app.post('/contact', contacts.create);
 	app.get('/contact/:id/edit', contacts.edit);
 	app.put('/contact/:id', contacts.update);
-	app.delete('/', function(req, res) {
-		res.send('DELETE request to homepage');
-	})
+	app.delete('/contact/:id', contacts.destroy)
 };
