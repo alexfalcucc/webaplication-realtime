516861a5ddb04d4095ffd4b1272ee28becee526c
alexfalcucc <alex.falcucci@gmail.com>
2015-01-31 18:38:37 -0200

    page not found its working

--- alexsander-ntalk/app.js
+++ alexsander-ntalk/app.js
@@ -4,27 +4,39 @@ var express = require('express'),
   cookieParser = require('cookie-parser'),
   expressSession = require('express-session'),
   methodOverride = require('method-override'),
+  error = require('./middlewares/error'),
   app = express();
 
 app.set('views', __dirname + '/views');
+
 app.set('view engine', 'ejs');
+
 app.use(cookieParser());
+
 app.use(expressSession({
   secret: 'alexsander-ntalk',
   resave: false,
   saveUninitialized: false
 }));
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(methodOverride('_method'));
+
 app.use(express.static(__dirname + '/public'));
 
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
+
+app.use(error.notFound);
+
+app.use(error.serverError);
 
 app.listen(3000, function() {
   console.log("Ntalk it's working!");
